a08388c0a1a02058fe4a79551430d3f040e7cf29
sinaon.bro <sinaon.bro@gmail.com>
2021-07-15 01:10:58 +0900

    Create rss-reader.js

---
+++ rss-app/rss-reader.js
@@ -0,0 +1,18 @@
+let Parser = require('rss-parser');
+let parser = new Parser();
+
+async function feedTitleList(url) {
+
+    let feed = await parser.parseURL(url);
+    
+    console.log(feed.title);
+
+    feed.items.forEach(item => {
+		    console.log(item.title);
+    });
+
+};
+
+sampleUrl = 'https://www.yonhapnewstv.co.kr/category/news/headline/feed/';
+
+feedTitleList(sampleUrl);
