2916886bb8be8ed0e9887b18aeea3374f43695ba
sinaon.bro <sinaon.bro@gmail.com>
2021-07-25 00:59:33 +0900

    Removed empty lines

--- rss-app/rss-reader.js
+++ rss-app/rss-reader.js
@@ -2,7 +2,6 @@ let Parser = require('rss-parser');
 let parser = new Parser();
 
 async function feedTitleList(url) {
-
     let feed = await parser.parseURL(url);
     
     console.log(feed.title);
@@ -10,7 +9,6 @@ async function feedTitleList(url) {
     feed.items.forEach(item => {
 		    console.log(item.title);
     });
-
 };
 
 sampleUrl = 'https://www.yonhapnewstv.co.kr/category/news/headline/feed/';
